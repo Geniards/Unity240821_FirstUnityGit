5ec7574aa02688bf9a53a427781032155927c736
realhyj <9k9k9k9k@naver.com>
2024-08-21 17:56:43 +0900

    Move 내용 작성

--- Unity240821_FirstUnityGit/Assets/HYJ/Script/Move.cs
+++ Unity240821_FirstUnityGit/Assets/HYJ/Script/Move.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
 
+    public int jumpPower;
     public Rigidbody rigid;
 
-    
-    void Update()
+    private void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody>();
+    }
+    private void Update() // 프레임마다 호출 될때
+    {
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigid.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        rigid.AddForce(new Vector3(h, 0, v), ForceMode.Impulse);
     }
 }
