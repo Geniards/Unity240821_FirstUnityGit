42406c91dcbe67643adc7bf7fb397f999c64f4a2
anMois <kew1672@naver.com>
2024-08-21 17:38:31 +0900

    [feat] : move 코드 생성
    move 코드 생성

---
+++ Unity240821_FirstUnityGit/Assets/PJS/Move.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed;
+    public Vector3 obj;
+
+    private void Start()
+    {
+        int iNum = 1;
+        float fNum = 0.0f;
+    }
+
+
+    private void Update()
+    {
+        
+    }
+}
