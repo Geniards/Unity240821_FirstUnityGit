fff18fd31790645611961b31ce2a27e8c5489340
anMois <kew1672@naver.com>
2024-08-21 18:01:53 +0900

    [fix] move -> moveTest 이름변경
    move -> moveTest 이름변경

--- Unity240821_FirstUnityGit/Assets/PJS/MoveTest.cs
+++ Unity240821_FirstUnityGit/Assets/PJS/MoveTest.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move : MonoBehaviour
+public class MoveTest : MonoBehaviour
 {
     public float speed;
     public Vector3 obj;
